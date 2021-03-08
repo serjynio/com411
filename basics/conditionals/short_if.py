0e3212f7b92376196482a937f67a577cdcd69bde
repl.it user <replituser@example.com>
2021-03-08 20:52:14 +0000

    new

---
+++ basics/conditionals/short_if.py
@@ -0,0 +1,7 @@
+name = input("What is your name:")
+age = int(input("What is your age"))
+
+if (age >= 18):
+  print("Hello " , name, "! We are happy to serve you alcohol")
+else:
+  print("Hello " + name + "! Please come back later")
---
+++ basics/input/data_types.py
@@ -0,0 +1,4 @@
+name = input("What is your name human?")
+age = int(input("How old are you (in years) ?"))
+heigh = float(input("How tall are you (in meters) ?"))
+weigh = float(input("How much do you weigh (in kilograms) ?"))
