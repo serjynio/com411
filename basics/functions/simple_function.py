d3023a1cab4f5eebcbee6b24a418f4f1a271f8e9
repl.it user <replituser@example.com>
2021-03-13 01:10:21 +0000

    dhasdhaj

---
+++ basics/functions/simple_function.py
@@ -0,0 +1,13 @@
+# The function
+def listen():
+
+    # Ask user for the sound 
+    print("What sound did I hear?")
+    sound = input() 
+
+    # Display message
+    print("\nThat was a loud", sound, "!")
+
+
+# Call to function
+listen()
