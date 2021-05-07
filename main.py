3a16b081a966b6ab5b4d79bd0a769c827b40cf2c
repl.it user <replituser@example.com>
2021-05-07 16:09:13 +0000

    tasks.....

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-def search(file_path):
-  print("Searching...")
-  with open(file_path) as file:
-    for line in file.readlines():
-      print(f"Looked in {line.strip()}")
-  print("...Done!")
+def list_entities():
+  entities =['Mercury','Venus', 'Earth','Mars','Jupiter','Saturn','Uranus','Neptune', 'Pluto']
+  cols = [4,5]
+  for list11 in entities:
+    if len(cols) == 0:
+      for j in list11:
+        print(j, end=", ")
+      print("\n")
+    else:
+      for i in cols:
+        print(entities[i], end=", ")
+      return
+list_entities()
 
-def run():
-  search("data/files/txt/locations.txt")
 
-run()
+
+
+'''def save():
+  print("How do you want to save the data?")
+  options = ['Export as CSV','Export as TXT','Export as JSON']
+  for index, options in enumerate(options, start =1):
+    print(index, options)
+  u_input = int(input())
+
+  if u_input == range(1, len(options)+1):
+      print(u_input)
+      return u_input
+  else:
+      print('bad command')
+      return None
+
+save()'''
